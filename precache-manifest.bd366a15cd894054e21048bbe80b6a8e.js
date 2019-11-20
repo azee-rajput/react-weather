34b424da3efba06297ac4f35dd3d9ccefe663d8b
azee-rajput <azee.rajput.7@gmail.com>
2019-11-20 18:58:38 +0500

    Updates

---
+++ precache-manifest.bd366a15cd894054e21048bbe80b6a8e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e0488d248250c7f40e76b35e6e83bab",
+    "url": "/react-weather/index.html"
+  },
+  {
+    "revision": "c0ee7499f939a6603a34",
+    "url": "/react-weather/static/css/main.282c0366.chunk.css"
+  },
+  {
+    "revision": "7f02a9dcea5239446ae2",
+    "url": "/react-weather/static/js/2.d1971cfe.chunk.js"
+  },
+  {
+    "revision": "c0ee7499f939a6603a34",
+    "url": "/react-weather/static/js/main.e6099a29.chunk.js"
+  },
+  {
+    "revision": "b4d7c30564fb220d17f5",
+    "url": "/react-weather/static/js/runtime-main.7602acae.js"
+  }
+]);
